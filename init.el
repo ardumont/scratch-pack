1f985d7cd00b0dedb335243ee6d0d0106ac6ba33
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-08-01 14:22:28 +0200

    Removing not needed code

--- init.el
+++ init.el
@@ -8,18 +8,4 @@
 ;; delay before displaying the help message
 (setq ac-quick-help-delay 0.3)
 
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(require 'el-get)
-
-(setq el-get-verbose t)
-
-(el-get-install "goto-last-change")
-
-;; personal recipes
-(setq el-get-sources
-      '((:name   goto-last-change
-         :before (global-set-key (kbd "C-x C-/") 'goto-last-change))))
-
-(el-get 'sync)
-
 (live-load-config-file "bindings.el")
