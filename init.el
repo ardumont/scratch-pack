f7372740edbacc9313a88929fa51ff5402682d30
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-08-01 14:14:18 +0200

    Forgot deps

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 (install-packs '(;; some playtime with graphviz
                  graphviz-dot-mode
-                 htmlize))
+                 htmlize
+                 auto-complete))
+
+(require 'auto-complete)
 
 ;; delay before displaying the help message
 (setq ac-quick-help-delay 0.3)
