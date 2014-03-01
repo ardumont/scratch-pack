8ef758fff258a844bfb49dc2f81eee084e599831
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-01 14:28:06 +0100

    Add headers

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;;; install-packages-pack.el --- Install routine
+
+;;; Commentary:
+
+;;; Code:
+
 (install-packs '(;; some playtime with graphviz
                  graphviz-dot-mode
                  htmlize
@@ -8,3 +14,4 @@
 ;; delay before displaying the help message
 (setq ac-quick-help-delay 0.3)
 
+;;; install-packages-pack.el ends here
