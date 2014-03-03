b982e1f5e295e1433f627346f9b6aa2cbb176e1e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-04 00:07:15 +0100

    Add guide-key

--- init.el
+++ init.el
@@ -6,11 +6,20 @@
 
 (install-packs '(graphviz-dot-mode
                  htmlize
-                 auto-complete))
+                 auto-complete
+                 guide-key))
 
 (require 'auto-complete)
 
 ;; delay before displaying the help message
 (setq ac-quick-help-delay 0.3)
+
+(require 'guide-key)
+(setq guide-key/idle-delay 0.1)                ;; Delay in seconds before guide
+                                               ;; buffer is displayed.
+(setq guide-key/guide-key-sequence '("C-c"))   ;; key sequence to learn
+(setq guide-key/recursive-key-sequence-flag t)
+
+(guide-key-mode 1)                             ;; Enable guide-key-mode
 
 ;;; scratch-pack.el ends here
