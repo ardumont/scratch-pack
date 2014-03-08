b6b1b8a522fd6d7f76ebd0409540161061b68c9c
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-08 17:57:52 +0100

    Add the `C-x` prefix key

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
 (require 'guide-key)
 (setq guide-key/idle-delay 0.8)                ;; Delay in seconds before guide
                                                ;; buffer is displayed.
-(setq guide-key/guide-key-sequence '("C-c"))   ;; key sequence to learn
+(setq guide-key/guide-key-sequence '("C-c" "C-x"))   ;; key sequence to learn
 (setq guide-key/recursive-key-sequence-flag t)
 
 (guide-key-mode 1)                             ;; Enable guide-key-mode
