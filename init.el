47d79e89ce0628f6da2ba5a1191661f8640dd862
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-08 15:39:20 +0200

    Extract help mode inside a dedicated help-pack

--- init.el
+++ init.el
@@ -6,21 +6,12 @@
 
 (install-packs '(graphviz-dot-mode
                  htmlize
-                 auto-complete
-                 guide-key))
+                 auto-complete))
 
 (require 'auto-complete)
 
 ;; delay before displaying the help message
 (setq ac-quick-help-delay 0.3)
-
-(require 'guide-key)
-(setq guide-key/idle-delay 0.8)                ;; Delay in seconds before guide
-                                               ;; buffer is displayed.
-(setq guide-key/guide-key-sequence '("C-c" "C-x"))   ;; key sequence to learn
-(setq guide-key/recursive-key-sequence-flag t)
-
-(guide-key-mode 1)                             ;; Enable guide-key-mode
 
 (require 'graphviz-dot-mode)
 
